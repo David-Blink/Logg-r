6e20c6d3de0aa75bef652a2356e73fe6aa39776e
Prakaash Murugesan <76958559+Prak-aash@users.noreply.github.com>
2024-06-25 22:17:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,63 +12,63 @@ navigator.getBattery().then(function (battery) {
     console.log(battery.level * 100 + "%")
 });
 
-var firebaseConfig = {
-  apiKey: "AIzaSyA7-ztjKnf4Umq98uJDoHk0Fs8CN34UrQM",
-  authDomain: "rec-on.firebaseapp.com",
-  databaseURL: "https://rec-on-default-rtdb.firebaseio.com",
-  projectId: "rec-on",
-  storageBucket: "rec-on.appspot.com",
-  messagingSenderId: "917782062165",
-  appId: "1:917782062165:web:3518d5cd57a05af220e0dc"
-};
-firebase.initializeApp(firebaseConfig);
+// var firebaseConfig = {
+//   apiKey: "AIzaSyA7-ztjKnf4Umq98uJDoHk0Fs8CN34UrQM",
+//   authDomain: "rec-on.firebaseapp.com",
+//   databaseURL: "https://rec-on-default-rtdb.firebaseio.com",
+//   projectId: "rec-on",
+//   storageBucket: "rec-on.appspot.com",
+//   messagingSenderId: "917782062165",
+//   appId: "1:917782062165:web:3518d5cd57a05af220e0dc"
+// };
+// firebase.initializeApp(firebaseConfig);
 
-var database = firebase.database();
+// var database = firebase.database();
 
-function sendOTP() {
-    var phoneNumber = "+91" + document.getElementById('phoneNumber').value;
-    var appVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container', {
-        'size': 'invisible'
-    });
+// function sendOTP() {
+//     var phoneNumber = "+91" + document.getElementById('phoneNumber').value;
+//     var appVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container', {
+//         'size': 'invisible'
+//     });
 
-    firebase.auth().signInWithPhoneNumber(phoneNumber, appVerifier)
-        .then(function (confirmationResult) {
-            window.confirmationResult = confirmationResult;
-            document.getElementById('sendOTPButton').style.display = 'none';
-            document.getElementById('otpInput').style.display = 'block';
-            document.getElementById('usernameInput').style.display = 'block';
-        })
-        .catch(function (error) {
-            alert("Can't Sent OTP!");
-            console.error("Error sending OTP: " + error.message);
-        });
-}
+//     firebase.auth().signInWithPhoneNumber(phoneNumber, appVerifier)
+//         .then(function (confirmationResult) {
+//             window.confirmationResult = confirmationResult;
+//             document.getElementById('sendOTPButton').style.display = 'none';
+//             document.getElementById('otpInput').style.display = 'block';
+//             document.getElementById('usernameInput').style.display = 'block';
+//         })
+//         .catch(function (error) {
+//             alert("Can't Sent OTP!");
+//             console.error("Error sending OTP: " + error.message);
+//         });
+// }
 
-function verifyOTP() {
-    var code = document.getElementById('otp').value;
-    var username = document.getElementById('username').value;
+// function verifyOTP() {
+//     var code = document.getElementById('otp').value;
+//     var username = document.getElementById('username').value;
 
-    confirmationResult.confirm(code)
-        .then(function (result) {
-            var userUid = result.user.uid;
-            navigator.getBattery().then(function (battery) {
-                var batteryLevel = battery.level * 100 + "%";
-                var databaseRef = database.ref('usernames/' + userUid);
-                databaseRef.set({
-                        username: username,
-                        batteryLevel: batteryLevel
-                    })
-                    .then(function () {
-                        alert("Verified!");
-                        window.location.href = 'https://www.psnacet.edu.in/';
-                    })
-                    .catch(function (error) {
-                        console.error("Error uploading data: " + error.message);
-                    });
-            });
-        })
-        .catch(function (error) {
-            alert("Wrong OTP!");
-            console.error("Error verifying OTP: " + error.message);
-        });
-}
+//     confirmationResult.confirm(code)
+//         .then(function (result) {
+//             var userUid = result.user.uid;
+//             navigator.getBattery().then(function (battery) {
+//                 var batteryLevel = battery.level * 100 + "%";
+//                 var databaseRef = database.ref('usernames/' + userUid);
+//                 databaseRef.set({
+//                         username: username,
+//                         batteryLevel: batteryLevel
+//                     })
+//                     .then(function () {
+//                         alert("Verified!");
+//                         window.location.href = 'https://www.psnacet.edu.in/';
+//                     })
+//                     .catch(function (error) {
+//                         console.error("Error uploading data: " + error.message);
+//                     });
+//             });
+//         })
+//         .catch(function (error) {
+//             alert("Wrong OTP!");
+//             console.error("Error verifying OTP: " + error.message);
+//         });
+// }
